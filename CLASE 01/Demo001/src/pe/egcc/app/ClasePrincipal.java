580d223bdb5b6d29a939cd6150a7cc91f0c15859
Javier Paulino <diegojava01@gmail.com>
2016-04-24 18:31:01 -0500

    subiendo clase 01

---
+++ CLASE 01/Demo001/src/pe/egcc/app/ClasePrincipal.java
@@ -0,0 +1,15 @@
+
+package pe.egcc.app;
+
+import pe.egcc.app.view.SumaView;
+
+
+public class ClasePrincipal {
+
+    
+    public static void main(String[] args) {
+        SumaView obj = new SumaView();
+        obj.setVisible(true);
+    }
+    
+}
---
+++ CLASE 01/Demo001/src/pe/egcc/app/service/MateService.java
@@ -0,0 +1,15 @@
+
+package pe.egcc.app.service;
+
+/**
+ *
+ * @author Alumno
+ */
+public class MateService {
+    
+    public int sumar(int n1,int n2){
+      return (n1+n2);
+    }
+    
+    
+}
